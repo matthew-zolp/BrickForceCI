9ef22443c3ee8fd6ac99377aab31c807b1124b54
Matt Zolp <Zolp_Matthew@bah.com>
2025-01-16 11:39:44 -0600

    Add Jest support and modify validate action

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,22 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
+const collectCoverageFrom = [];
+collectCoverageFrom.push('./sfdx-source/salesforce-scheduling/main/default/lwc/**/*.{js,jsx}');
 
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    setupFilesAfterEnv,
+    collectCoverageFrom: collectCoverageFrom,
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+    moduleNameMapper: {
+        '\\.css$': '<rootDir>/test/jest-mocks/c/styleMock.js', // Mock all .css imports
+        '^lightning/modal': '<rootDir>/test/jest-mocks/lightning/modal',
+        '^lightning/navigation$': '<rootDir>/test/jest-mocks/lightning/navigation',
+        '^lightning/modal$': '<rootDir>/test/jest-mocks/lightning/modal',
+        '^lightning/uiRecordApi$': '<rootDir>/test/jest-mocks/lightning/uiRecordApi',
+        '^lightning/platformShowToastEvent$': '<rootDir>/test/jest-mocks/lightning/platformShowToastEvent',
+        '^c/pubsub$': '<rootDir>/test/jest-mocks/c/pubsub',
+        '^omnistudio/pubsub$': '<rootDir>/test/jest-mocks/omnistudio/pubsub'
+    }
 };
